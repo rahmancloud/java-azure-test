41ec2ef40c4abbd0c8ec2b0b7233db11c11ca1be
rahmancloud <amanyus@gmail.com>
2024-03-16 03:34:33 +0000

    Initial commit

---
+++ src/main/java/com/fabrikam/App.java
@@ -0,0 +1,94 @@
+package com.fabrikam;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.credential.AzureCliCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.AzureAuthorityHosts;
+import com.azure.identity.AzureCliCredential;
+import com.azure.identity.AzureCliCredentialBuilder;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.storage.models.PublicEndpoints;
+import com.azure.resourcemanager.storage.models.StorageAccount;
+import com.azure.resourcemanager.storage.models.StorageAccountKey;
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.PublicAccessType;
+import com.azure.storage.common.StorageSharedKeyCredential;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            // TokenCredential tokenCredential = new DefaultAzureCredentialBuilder()
+            //         .authorityHost(AzureAuthorityHosts.AZURE_PUBLIC_CLOUD)
+            //         .build();
+
+            AzureCliCredential cliCredential = new AzureCliCredentialBuilder().build();
+            // Use the Azure CLI credential to authenticate.
+
+            // If you don't set the tenant ID and subscription ID via environment variables,
+            // change to create the Azure profile with tenantId, subscriptionId, and Azure
+            // environment.
+            AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+
+            AzureResourceManager azureResourceManager = AzureResourceManager.configure()
+                    .withLogLevel(HttpLogDetailLevel.BASIC)
+                    .authenticate(cliCredential, profile)
+                    .withDefaultSubscription();
+
+            // Create a new storage account.
+            String storageAccountName = "abdulrahmanyusmadi123";
+            StorageAccount storage = azureResourceManager.storageAccounts().define(storageAccountName)
+                    .withRegion(Region.US_WEST2)
+                    .withNewResourceGroup("sampleStorageResourceGroup")
+                    .create();
+
+            // Create a storage container to hold the file.
+            List<StorageAccountKey> keys = storage.getKeys();
+            PublicEndpoints endpoints = storage.endPoints();
+            String accountName = storage.name();
+            String accountKey = keys.get(0).value();
+            String endpoint = endpoints.primary().blob();
+
+            StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, accountKey);
+
+            BlobServiceClient storageClient = new BlobServiceClientBuilder()
+                    .endpoint(endpoint)
+                    .credential(credential)
+                    .buildClient();
+
+            // Container name must be lowercase.
+            BlobContainerClient blobContainerClient = storageClient.getBlobContainerClient("helloazure");
+            // blobContainerClient.create();
+
+            // Make the container public.
+            blobContainerClient.setAccessPolicy(PublicAccessType.CONTAINER, null);
+
+            // Write a blob to the container.
+            String fileName = "helloazure5.txt";
+            String textNew = "Hello Azure";
+
+            BlobClient blobClient = blobContainerClient.getBlobClient(fileName);
+            InputStream is = new ByteArrayInputStream(textNew.getBytes());
+            blobClient.upload(is, textNew.length());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
